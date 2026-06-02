acad2014f81e7f5c64a30def38215bd86ab54eee
Swagat Pradhan <swagat9994@gmail.com>
2026-06-02 23:32:53 +0530

    Updated

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from backend.routes import router
 
 
 app = FastAPI(
     title="Brain Tumor Detection API",
     version="1.0"
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 app.include_router(
